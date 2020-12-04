3d50b1ea974bbf73868a354addccf5471b73733b
jay_wu <jay_wu@gss.com.tw>
2020-12-04 23:28:30 +0800

    ★ Android 設定

---
+++ android/app/src/main/kotlin/com/wjprogramer/flutter_push_notification_1/Application.kt
@@ -0,0 +1,19 @@
+package com.wjprogramer.flutter_push_notification_1
+
+import io.flutter.app.FlutterApplication
+import io.flutter.plugin.common.PluginRegistry
+import io.flutter.plugin.common.PluginRegistry.PluginRegistrantCallback
+import io.flutter.plugins.firebasemessaging.FlutterFirebaseMessagingService
+import io.flutter.plugins.firebasemessaging.FirebaseMessagingPlugin
+
+class Application : FlutterApplication(), PluginRegistrantCallback {
+    override fun onCreate() {
+        super.onCreate()
+        FlutterFirebaseMessagingService.setPluginRegistrant(this)
+    }
+
+    override fun registerWith(registry: PluginRegistry) {
+//        FirebaseCloudMessagingPluginRegistrant.registerWith(registry)
+        FirebaseMessagingPlugin.registerWith(registry.registrarFor("io.flutter.plugins.firebasemessaging.FirebaseMessagingPlugin"));
+    }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,143 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'dart:async';
+
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
+Future<dynamic> myBackgroundMessageHandler(Map<String, dynamic> message) async {
+  if (message.containsKey('data')) {
+    // Handle data message
+    final dynamic data = message['data'];
+  }
+
+  if (message.containsKey('notification')) {
+    // Handle notification message
+    final dynamic notification = message['notification'];
+  }
+
+  // Or do other work.
+}
+
+final Map<String, Item> _items = <String, Item>{};
+Item _itemForMessage(Map<String, dynamic> message) {
+  final dynamic data = message['data'] ?? message;
+  final String itemId = data['id'];
+  final Item item = _items.putIfAbsent(itemId, () => Item(itemId: itemId))
+    .._matchTeam = data['matchTeam']
+    .._score = data['score'];
+  return item;
+}
+
+class Item {
+  Item({this.itemId});
+  final String itemId;
+
+  StreamController<Item> _controller = StreamController<Item>.broadcast();
+  Stream<Item> get onChanged => _controller.stream;
+
+  String _matchTeam;
+  String get matchTeam => _matchTeam;
+  set matchTeam(String value) {
+    _matchTeam = value;
+    _controller.add(this);
+  }
+
+  String _score;
+  String get score => _score;
+  set score(String value) {
+    _score = value;
+    _controller.add(this);
+  }
+
+  static final Map<String, Route<void>> routes = <String, Route<void>>{};
+  Route<void> get route {
+    final String routeName = '/detail/$itemId';
+    return routes.putIfAbsent(
+      routeName,
+          () => MaterialPageRoute<void>(
+        settings: RouteSettings(name: routeName),
+        builder: (BuildContext context) => DetailPage(itemId),
+      ),
+    );
+  }
+}
+
+class DetailPage extends StatefulWidget {
+  DetailPage(this.itemId);
+  final String itemId;
+  @override
+  _DetailPageState createState() => _DetailPageState();
+}
+
+class _DetailPageState extends State<DetailPage> {
+  Item _item;
+  StreamSubscription<Item> _subscription;
+
+  @override
+  void initState() {
+    super.initState();
+    _item = _items[widget.itemId];
+    _subscription = _item.onChanged.listen((Item item) {
+      if (!mounted) {
+        _subscription.cancel();
+      } else {
+        setState(() {
+          _item = item;
+        });
+      }
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text("Match ID ${_item.itemId}"),
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.all(20.0),
+          child: Card(
+            child: Container(
+                padding: EdgeInsets.all(10.0),
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.fromLTRB(0, 0, 0, 10),
+                      child: Column(
+                        children: <Widget>[
+                          Text('Today match:', style: TextStyle(color: Colors.black.withOpacity(0.8))),
+                          Text( _item.matchTeam, style: Theme.of(context).textTheme.title)
+                        ],
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.fromLTRB(0, 0, 0, 10),
+                      child: Column(
+                        children: <Widget>[
+                          Text('Score:', style: TextStyle(color: Colors.black.withOpacity(0.8))),
+                          Text( _item.score, style: Theme.of(context).textTheme.title)
+                        ],
+                      ),
+                    ),
+                  ],
+                )
+            ),
+          ),
+        ),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -21,10 +157,6 @@ class MyApp extends StatelessWidget {
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -50,17 +182,82 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  bool _topicButtonsDisabled = false;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
+  final TextEditingController _topicController =
+  TextEditingController(text: 'topic');
+
+  Widget _buildDialog(BuildContext context, Item item) {
+    return AlertDialog(
+      content: Text("${item.matchTeam} with score: ${item.score}"),
+      actions: <Widget>[
+        FlatButton(
+          child: const Text('CLOSE'),
+          onPressed: () {
+            Navigator.pop(context, false);
+          },
+        ),
+        FlatButton(
+          child: const Text('SHOW'),
+          onPressed: () {
+            Navigator.pop(context, true);
+          },
+        ),
+      ],
+    );
+  }
+
+  void _showItemDialog(Map<String, dynamic> message) {
+    showDialog<bool>(
+      context: context,
+      builder: (_) => _buildDialog(context, _itemForMessage(message)),
+    ).then((bool shouldNavigate) {
+      if (shouldNavigate == true) {
+        _navigateToItemDetail(message);
+      }
     });
+  }
+
+  void _navigateToItemDetail(Map<String, dynamic> message) {
+    final Item item = _itemForMessage(message);
+    // Clear away dialogs
+    Navigator.popUntil(context, (Route<dynamic> route) => route is PageRoute);
+    if (!item.route.isCurrent) {
+      Navigator.push(context, item.route);
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _firebaseMessaging.configure(
+      onMessage: (Map<String, dynamic> message) async {
+        print("onMessage: $message");
+        _showItemDialog(message);
+      },
+      onBackgroundMessage: myBackgroundMessageHandler,
+      onLaunch: (Map<String, dynamic> message) async {
+        print("onLaunch: $message");
+        _navigateToItemDetail(message);
+      },
+      onResume: (Map<String, dynamic> message) async {
+        print("onResume: $message");
+        _navigateToItemDetail(message);
+      },
+    );
+    _firebaseMessaging.requestNotificationPermissions(
+        const IosNotificationSettings(
+            sound: true, badge: true, alert: true, provisional: true));
+    _firebaseMessaging.onIosSettingsRegistered
+        .listen((IosNotificationSettings settings) {
+      print("Settings registered: $settings");
+    });
+    _firebaseMessaging.getToken().then((String token) {
+      assert(token != null);
+      print("Push Messaging token: $token");
+    });
+    _firebaseMessaging.subscribeToTopic("matchscore");
   }
 
   @override
@@ -75,43 +272,283 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('My Flutter FCM'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.all(20.0),
+          child: Card(
+            child: Container(
+                padding: EdgeInsets.all(10.0),
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.fromLTRB(0, 0, 0, 10),
+                      child: Column(
+                        children: <Widget>[
+                          Text('Welcome to this Flutter App:', style: TextStyle(color: Colors.black.withOpacity(0.8))),
+                          Text('You already subscribe to the matchscore topic', style: Theme.of(context).textTheme.title)
+                        ],
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.fromLTRB(0, 0, 0, 10),
+                      child: Column(
+                        children: <Widget>[
+                          Text('Now you will receive the push notification from the matchscore topics', style: TextStyle(color: Colors.black.withOpacity(0.8)))
+                        ],
+                      ),
+                    ),
+                  ],
+                )
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: MyHomePage(),
+    ),
+  );
+}
+
+// final Map<String, Item> _items = <String, Item>{};
+// Item _itemForMessage(Map<String, dynamic> message) {
+//   final dynamic data = message['data'] ?? message;
+//   final String itemId = data['id'];
+//   final Item item = _items.putIfAbsent(itemId, () => Item(itemId: itemId))
+//     ..status = data['status'];
+//   return item;
+// }
+//
+// class Item {
+//   Item({this.itemId});
+//   final String itemId;
+//
+//   StreamController<Item> _controller = StreamController<Item>.broadcast();
+//   Stream<Item> get onChanged => _controller.stream;
+//
+//   String _status;
+//   String get status => _status;
+//   set status(String value) {
+//     _status = value;
+//     _controller.add(this);
+//   }
+//
+//   static final Map<String, Route<void>> routes = <String, Route<void>>{};
+//   Route<void> get route {
+//     final String routeName = '/detail/$itemId';
+//     return routes.putIfAbsent(
+//       routeName,
+//           () => MaterialPageRoute<void>(
+//         settings: RouteSettings(name: routeName),
+//         builder: (BuildContext context) => DetailPage(itemId),
+//       ),
+//     );
+//   }
+// }
+//
+// class DetailPage extends StatefulWidget {
+//   DetailPage(this.itemId);
+//   final String itemId;
+//   @override
+//   _DetailPageState createState() => _DetailPageState();
+// }
+//
+// class _DetailPageState extends State<DetailPage> {
+//   Item _item;
+//   StreamSubscription<Item> _subscription;
+//
+//   @override
+//   void initState() {
+//     super.initState();
+//     _item = _items[widget.itemId];
+//     _subscription = _item.onChanged.listen((Item item) {
+//       if (!mounted) {
+//         _subscription.cancel();
+//       } else {
+//         setState(() {
+//           _item = item;
+//         });
+//       }
+//     });
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text("Item ${_item.itemId}"),
+//       ),
+//       body: Material(
+//         child: Center(child: Text("Item status: ${_item.status}")),
+//       ),
+//     );
+//   }
+// }
+//
+// class PushMessagingExample extends StatefulWidget {
+//   @override
+//   _PushMessagingExampleState createState() => _PushMessagingExampleState();
+// }
+//
+// class _PushMessagingExampleState extends State<PushMessagingExample> {
+//   String _homeScreenText = "Waiting for token...";
+//   bool _topicButtonsDisabled = false;
+//
+//   final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
+//   final TextEditingController _topicController =
+//   TextEditingController(text: 'topic');
+//
+//   Widget _buildDialog(BuildContext context, Item item) {
+//     return AlertDialog(
+//       content: Text("Item ${item.itemId} has been updated"),
+//       actions: <Widget>[
+//         FlatButton(
+//           child: const Text('CLOSE'),
+//           onPressed: () {
+//             Navigator.pop(context, false);
+//           },
+//         ),
+//         FlatButton(
+//           child: const Text('SHOW'),
+//           onPressed: () {
+//             Navigator.pop(context, true);
+//           },
+//         ),
+//       ],
+//     );
+//   }
+//
+//   void _showItemDialog(Map<String, dynamic> message) {
+//     showDialog<bool>(
+//       context: context,
+//       builder: (_) => _buildDialog(context, _itemForMessage(message)),
+//     ).then((bool shouldNavigate) {
+//       if (shouldNavigate == true) {
+//         _navigateToItemDetail(message);
+//       }
+//     });
+//   }
+//
+//   void _navigateToItemDetail(Map<String, dynamic> message) {
+//     final Item item = _itemForMessage(message);
+//     // Clear away dialogs
+//     Navigator.popUntil(context, (Route<dynamic> route) => route is PageRoute);
+//     if (!item.route.isCurrent) {
+//       Navigator.push(context, item.route);
+//     }
+//   }
+//
+//   @override
+//   void initState() {
+//     super.initState();
+//     _firebaseMessaging.configure(
+//       onMessage: (Map<String, dynamic> message) async {
+//         print("onMessage: $message");
+//         _showItemDialog(message);
+//       },
+//       onLaunch: (Map<String, dynamic> message) async {
+//         print("onLaunch: $message");
+//         _navigateToItemDetail(message);
+//       },
+//       onResume: (Map<String, dynamic> message) async {
+//         print("onResume: $message");
+//         _navigateToItemDetail(message);
+//       },
+//     );
+//     _firebaseMessaging.requestNotificationPermissions(
+//         const IosNotificationSettings(
+//             sound: true, badge: true, alert: true, provisional: true));
+//     _firebaseMessaging.onIosSettingsRegistered
+//         .listen((IosNotificationSettings settings) {
+//       print("Settings registered: $settings");
+//     });
+//     _firebaseMessaging.getToken().then((String token) {
+//       assert(token != null);
+//       setState(() {
+//         _homeScreenText = "Push Messaging token: $token";
+//       });
+//       print(_homeScreenText);
+//     });
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//         appBar: AppBar(
+//           title: const Text('Push Messaging Demo'),
+//         ),
+//         // For testing -- simulate a message being received
+//         floatingActionButton: FloatingActionButton(
+//           onPressed: () => _showItemDialog(<String, dynamic>{
+//             "data": <String, String>{
+//               "id": "2",
+//               "status": "out of stock",
+//             },
+//           }),
+//           tooltip: 'Simulate Message',
+//           child: const Icon(Icons.message),
+//         ),
+//         body: Material(
+//           child: Column(
+//             children: <Widget>[
+//               Center(
+//                 child: Text(_homeScreenText),
+//               ),
+//               Row(children: <Widget>[
+//                 Expanded(
+//                   child: TextField(
+//                       controller: _topicController,
+//                       onChanged: (String v) {
+//                         setState(() {
+//                           _topicButtonsDisabled = v.isEmpty;
+//                         });
+//                       }),
+//                 ),
+//                 FlatButton(
+//                   child: const Text("subscribe"),
+//                   onPressed: _topicButtonsDisabled
+//                       ? null
+//                       : () {
+//                     _firebaseMessaging
+//                         .subscribeToTopic(_topicController.text);
+//                     _clearTopicText();
+//                   },
+//                 ),
+//                 FlatButton(
+//                   child: const Text("unsubscribe"),
+//                   onPressed: _topicButtonsDisabled
+//                       ? null
+//                       : () {
+//                     _firebaseMessaging
+//                         .unsubscribeFromTopic(_topicController.text);
+//                     _clearTopicText();
+//                   },
+//                 ),
+//               ])
+//             ],
+//           ),
+//         ));
+//   }
+//
+//   void _clearTopicText() {
+//     setState(() {
+//       _topicController.text = "";
+//       _topicButtonsDisabled = true;
+//     });
+//   }
+// }
+//
+// void main() {
+//   runApp(
+//     MaterialApp(
+//       home: PushMessagingExample(),
+//     ),
+//   );
+// }
